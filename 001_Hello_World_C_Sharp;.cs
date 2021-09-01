4eac602ab570ed6db849e96ee22505ddc6f8a4be
RudiReallife <40965986+RudiReallife@users.noreply.github.com>
2021-09-01 20:13:16 +0200

    Erstellen eines Hallo Welt Programm in C#
    Was ist die Main Methode?
    Konsoleneingabe
    Konsole offen halten

---
+++ 001_Hello_World_C_Sharp;.cs
@@ -0,0 +1,28 @@
+using System;
+
+
+namespace _001_Hallo_C_Sharp_Welt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             "Main" ist eine sog. Methode (früher auch: Funktion).
+             In C#, C++, C, Java uva. wird Main als erste Methode 
+             abgearbeitet.
+             */
+
+
+            // Der folgende Befehl übergibt einen String
+            // der durch Anführungszeichen gekennzeichnet wird
+            // "Hier steht ein Wort, Text oder Zeichenkette"
+            // in eine Ausgabezeile der Konsole
+            Console.WriteLine("Hallo Welt");
+
+            // Der folgende Befehl wartet auf eine Tastatureingabe
+            // und hält deswegen die Konsole offen.
+            Console.ReadLine();
+        }
+    }
+}
